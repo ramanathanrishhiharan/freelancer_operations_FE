09c3da8d6cd90d0e629b6a1e90502b8f562df413
ramanathanrishiharan <ramanathanrishiharan2003@gmail.com>
2026-07-02 13:27:29 +0530

    sidebar completed only ui

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,10 @@ import type { NextRequest } from "next/server";
 export function middleware(request:NextRequest){
     const token = request.cookies.get('token')?.value
 
-    if (!token){
-        return NextResponse.redirect(new URL("/auth",request.url))
+    // if (!token){
+    //     return NextResponse.redirect(new URL("/auth",request.url))
 
-    }
+    // }
     return NextResponse.next()
 }
 
